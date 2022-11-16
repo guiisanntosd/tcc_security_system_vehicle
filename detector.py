26b35f0101687f7a3a933c25a547b7f6f9ca69d3
Guilherme Santos <guilherme.snt99@gmail.com>
2022-11-15 22:21:56 -0300

    add list to object of interest

--- detector.py
+++ detector.py
@@ -19,6 +19,8 @@ class Detector:
         self.net.setInputMean((127.5, 127.5, 127.5))
         self.net.setInputSwapRB(True)
 
+        self.objects = [3, 4, 6, 8]
+
         self.readClasses()
 
     def sound(self):
@@ -47,66 +49,65 @@ class Detector:
             fps = 1 / (currentTime - startTime)
             startTime = currentTime
 
-            classIds, confs, bbox = self.net.detect(frame, confThreshold=0.5)
+            classIds, confs, bbox = self.net.detect(frame, confThreshold=0.55)
 
             bboxs = list(bbox)
             confidences = list(np.array(confs).reshape(1, -1)[0])
             confidences = list(map(float, confidences))
 
-            indices = cv2.dnn.NMSBoxes(
-                bboxs, confidences, score_threshold=0.5, nms_threshold=0.4
-            )
+            indices = cv2.dnn.NMSBoxes(bboxs, confidences, score_threshold=0.55, nms_threshold=0.2)
 
             if len(indices) != 0:
                 for i in range(0, len(indices)):
-                    self.sound()
-                    bbox = bboxs[np.squeeze(indices[i])]
-                    classConfidence = confidences[np.squeeze(indices[i])]
-                    classId = classIds[np.squeeze(indices[i])]
-                    classLabel = self.classes[classId]
-                    classColor = [int(c) for c in self.color[classId]]
+                    if classIds[0] in self.objects:
+                        self.sound()
+                        bbox = bboxs[np.squeeze(indices[i])]
+                        classConfidence = confidences[np.squeeze(indices[i])]
+                        classId = classIds[np.squeeze(indices[i])]
+                        classLabel = self.classes[classId]
+                        classColor = [int(c) for c in self.color[classId]]
 
-                    displayText = "{}: {:.2f}".format(classLabel, classConfidence * 100)
+                        displayText = "{}: {:.2f}".format(classLabel, classConfidence * 100)
 
-                    x, y, w, h = bbox
+                        x, y, w, h = bbox
 
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), classColor, 1)
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), classColor, 1)
 
-                    cv2.putText(
-                        frame,
-                        displayText,
-                        (x, y - 10),
-                        cv2.FONT_HERSHEY_PLAIN,
-                        1.5,
-                        classColor,
-                        2,
-                    )
+                        cv2.putText(
+                            frame,
+                            displayText,
+                            (x, y - 10),
+                            cv2.FONT_HERSHEY_PLAIN,
+                            1.5,
+                            classColor,
+                            2,
+                        )
 
-                    lineWidth = min(int(w * 0.3), int(h * 0.3))
+                        lineWidth = min(int(w * 0.3), int(h * 0.3))
 
-                    cv2.line(frame, (x, y), (x + lineWidth, y), classColor, 10)
-                    cv2.line(frame, (x, y), (x, y + lineWidth), classColor, 10)
+                        cv2.line(frame, (x, y), (x + lineWidth, y), classColor, 10)
+                        cv2.line(frame, (x, y), (x, y + lineWidth), classColor, 10)
 
-                    cv2.line(frame, (x + w, y), (x + w - lineWidth, y), classColor, 10)
-                    cv2.line(frame, (x + w, y), (x + w, y + lineWidth), classColor, 10)
+                        cv2.line(frame, (x + w, y), (x + w - lineWidth, y), classColor, 10)
+                        cv2.line(frame, (x + w, y), (x + w, y + lineWidth), classColor, 10)
 
-                    cv2.line(frame, (x, y + h), (x + lineWidth, y + h), classColor, 10)
-                    cv2.line(frame, (x, y + h), (x, y + h - lineWidth), classColor, 10)
+                        cv2.line(frame, (x, y + h), (x + lineWidth, y + h), classColor, 10)
+                        cv2.line(frame, (x, y + h), (x, y + h - lineWidth), classColor, 10)
 
-                    cv2.line(
-                        frame,
-                        (x + w, y + h),
-                        (x + w - lineWidth, y + h),
-                        classColor,
-                        10,
-                    )
-                    cv2.line(
-                        frame,
-                        (x + w, y + h),
-                        (x + w, y + h - lineWidth),
-                        classColor,
-                        10,
-                    )
+                        cv2.line(
+                            frame,
+                            (x + w, y + h),
+                            (x + w - lineWidth, y + h),
+                            classColor,
+                            10,
+                        )
+                        cv2.line(
+                            frame,
+                            (x + w, y + h),
+                            (x + w, y + h - lineWidth),
+                            classColor,
+                            10,
+                        )
 
             cv2.putText(
                 frame,
